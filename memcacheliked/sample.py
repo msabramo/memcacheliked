0bf9f1542ec850a97a7ac58a5009186e03ee6641
Marc Abramowitz <marc@marc-abramowitz.com>
2013-09-13 16:49:26 -0700

    Miscellaneous PEP8 and Python 3 (print function) cleanup
    --HG--
    branch : pep8_python3

--- memcacheliked/sample.py
+++ memcacheliked/sample.py
@@ -22,6 +22,7 @@
 
 from . import Memcacheliked, retrieval_command, storage_command, deletion_command, STORAGE_STATUS_STORED, DELETION_STATUS_DELETED, DELETION_STATUS_NOT_FOUND
 
+
 class SampleServer(Memcacheliked):
     def __init__(self):
         Memcacheliked.__init__(self)
@@ -30,25 +31,25 @@ class SampleServer(Memcacheliked):
 
     @retrieval_command
     def command_get(self, command_name, *keys):
-        print 'getting keys <%s>' % (keys,)
+        print('getting keys <%s>' % (keys,))
         return [(k, self.flags.get(k), self.data.get(k)) for k in keys]
 
     @storage_command
     def command_set(self, command_name, key, flags, exptime, value, *opts):
-        print 'setting key <%s> to <%s>' % (key, value,)
+        print('setting key <%s> to <%s>' % (key, value,))
         self.data[key] = value
         self.flags[key] = flags
         return STORAGE_STATUS_STORED
-    
+
     @deletion_command
     def command_delete(self, command_name, key, *opts):
-        print 'deleting key <%s>' % (key,)
+        print('deleting key <%s>' % (key,))
         if key not in self.data:
             return DELETION_STATUS_NOT_FOUND
         del self.data[key]
         del self.flags[key]
         return DELETION_STATUS_DELETED
-    
+
+
 if __name__ == "__main__":
     SampleServer().start()
-
