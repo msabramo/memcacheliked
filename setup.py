035e4a59dfa3f1c3e20749b40739f43eafb472a8
Vicious Red Beam <vicious.red.beam@gmail.com>
2013-09-16 01:47:58 +0100

    Version 0.2.1 for silly update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='memcacheliked',
-        version='0.2',
+        version='0.2.1',
         description='Simple framework for writing daemons using the memcache interface for storing data',
         author='Vicious Red Beam',
         author_email='vicious.red.beam@gmail.com',
